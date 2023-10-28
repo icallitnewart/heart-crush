7cb8c0d7494d8904622d56c5b96e86670881d6e8
icallitnewart <icallitnewart@gmail.com>
2023-10-28 23:35:20 +0900

    Chore: 프로젝트 생성 및 개발 환경 설정
    CRA(TS), ESLint (Airbnb), Prettier, Husky, lint-staged 설치 및 설정
    
    Resolves: #1

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react/recommended',
+		'airbnb',
+		'airbnb/hooks',
+		'airbnb-typescript',
+		'prettier',
+		'plugin:prettier/recommended',
+	],
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: ['.eslintrc.{js,cjs}'],
+			parserOptions: {
+				sourceType: 'script',
+			},
+		},
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		project: './tsconfig.json',
+	},
+	// ignorePatterns: ['.eslintrc.js'],
+	plugins: ['@typescript-eslint', 'react'],
+	rules: {},
+};
