7ddb0e98f37f4890074fc8c7233d6c29305bdb9c
icallitnewart <icallitnewart@gmail.com>
2023-11-16 12:28:40 +0900

    Design: 공통 폰트 및 색상 지정
    구글 폰트 import 및 색상 변수 지정
    
    Related to: #3

--- src/GlobalStyles.ts
+++ src/GlobalStyles.ts
@@ -3,6 +3,23 @@ import reset from 'styled-reset';
 
 const GlobalStyles = createGlobalStyle`
   ${reset}
+
+  :root {
+    --main-color-purple: #ADA2FF;
+    --main-color-yellow: #FBF0B2; //#ffe8ec;
+    --sub-color-blue: #CAEDFF; //#bad0ef;
+    --sub-color-purple: #D8B4F8; 
+    --sub-color-lightpink: #ffe8ec; 
+    --sub-color-pink: pink;//#b69cdc;
+
+    --main-font: Agbalumo;
+    --sub-font: Calistoga;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+  
 `;
 
 export default GlobalStyles;
