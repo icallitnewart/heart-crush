d07e913e545e33118d1a32acfca6c025e7fc6a58
icallitnewart <icallitnewart@gmail.com>
2024-02-12 01:35:24 +0900

    Refactor: rearrangeBoard 함수 가독성 및 유지보수성 개선
    - 단일 책임 원칙에 따라 함수 분리 및 추상화
    - 선언형 방식 도입으로 코드 명확성 향상
    - 깊은 복사와 데이터 변경의 최소화를 통해 불변성 유지 및 상태 업데이트 최적화
    
    Related to: #12

--- src/features/board/rearrangeBoard.ts
+++ src/features/board/rearrangeBoard.ts
@@ -1,62 +1,82 @@
 import { nanoid } from 'nanoid';
 
-import { BoardType, CellInfoType } from '../../types/board.type';
+import { BoardType, CellInfoType, ColumnType } from '../../types/board.type';
 import { FallingHeartsType } from '../../types/heart.type';
 import { categoriseHeartsByColumn } from '../../utils/heartSorting';
 import { getRandomHeart } from '../../utils/heartCreation';
 import { isHeartsMatchDownward } from '../../utils/heartMatching';
 
-function updateBoardAfterCrush(
-	board: BoardType,
-	heartsByColumn: Record<number, CellInfoType[]>,
-) {
-	const newBoard = board.map(column => ({
-		...column,
-		cells: [...column.cells],
-	}));
+function updateColumnWithFallingHearts(
+	column: ColumnType,
+	hearts: CellInfoType[],
+): ColumnType {
+	const newColumn = column;
 
-	Object.entries(heartsByColumn).forEach(
-		([columnIdx, fallingHearts]: [string, CellInfoType[]]) => {
-			const columnIndex = Number(columnIdx);
-			const rowIndexes = fallingHearts.map(heart => heart.position.rowIndex);
-			const minRowIndex = Math.min(...rowIndexes);
+	hearts.forEach(heart => {
+		const { rowIndex } = heart.position;
+		newColumn.cells[rowIndex] = {
+			id: heart.id,
+			heart: heart.heart,
+		};
+	});
 
-			// 위에서 떨어진 하트 채우기
-			fallingHearts.forEach(heart => {
-				const { rowIndex } = heart.position;
-
-				newBoard[columnIndex].cells[rowIndex] = {
-					id: heart.id,
-					heart: heart.heart,
-				};
-			});
-
-			// 새로운 하트 생성 및 채우기
-			for (let rowIndex = minRowIndex - 1; rowIndex >= 0; rowIndex -= 1) {
-				let randomHeart;
-				const position = {
-					columnIndex,
-					rowIndex,
-				};
-
-				do {
-					randomHeart = getRandomHeart();
-				} while (isHeartsMatchDownward(board, position, randomHeart));
-
-				newBoard[columnIndex].cells[rowIndex] = {
-					id: nanoid(),
-					heart: randomHeart,
-				};
-			}
-		},
-	);
-
-	return newBoard;
+	return newColumn;
 }
 
-function rearrangeBoard(board: BoardType, fallingHearts: FallingHeartsType) {
+function fillEmptyCellsWithNewHearts(
+	board: BoardType,
+	column: ColumnType,
+	columnIndex: number,
+	hearts: CellInfoType[],
+): ColumnType {
+	const newColumn = column;
+	const rowIndexes = hearts.map(heart => heart.position.rowIndex);
+	const minRowIndex = Math.min(...rowIndexes);
+
+	for (let rowIndex = minRowIndex - 1; rowIndex >= 0; rowIndex -= 1) {
+		let randomHeart;
+		const position = {
+			columnIndex,
+			rowIndex,
+		};
+
+		do {
+			randomHeart = getRandomHeart();
+		} while (isHeartsMatchDownward(board, position, randomHeart));
+
+		newColumn.cells[rowIndex] = {
+			id: nanoid(),
+			heart: randomHeart,
+		};
+	}
+
+	return newColumn;
+}
+
+function rearrangeBoard(
+	board: BoardType,
+	fallingHearts: FallingHeartsType,
+): BoardType {
 	const heartsByColumn = categoriseHeartsByColumn(fallingHearts);
-	const updatedBoard = updateBoardAfterCrush(board, heartsByColumn);
+
+	const updatedBoard = board.map((column, columnIndex) => {
+		let updatedColumn = {
+			...column,
+			cells: column.cells.map(cell => ({ ...cell })),
+		};
+		const hearts = heartsByColumn[columnIndex];
+		if (!hearts) return updatedColumn;
+
+		updatedColumn = updateColumnWithFallingHearts(updatedColumn, hearts);
+		updatedColumn = fillEmptyCellsWithNewHearts(
+			board,
+			updatedColumn,
+			columnIndex,
+			hearts,
+		);
+
+		return updatedColumn;
+	});
 
 	return updatedBoard;
 }
