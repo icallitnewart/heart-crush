23510c29adc0f0ed63815b385b060ee7abd9de1a
icallitnewart <icallitnewart@gmail.com>
2024-05-24 01:57:20 +0900

    Design: 팝업창 애니메이션 적용
    - 팝업창 나타날 때 fade-in 및 밑으로 내려오는 애니메이션 효과 적용
    - 팝업창 테두리 색깔 번갈아가며 변경되는 애니메이션 효과 적용
    
    Related to: #33

--- src/components/PopupBox.tsx
+++ src/components/PopupBox.tsx
@@ -1,5 +1,44 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const backgroundColorAnimation = keyframes`
+	0% {
+		background: repeating-linear-gradient(
+			45deg,
+			var(--main-color-yellow) 15px,
+			#888 15px,
+			var(--main-color-purple) 16px,
+			var(--main-color-purple) 30px,
+			#888 30px,
+			var(--main-color-yellow) 31px,
+			var(--main-color-yellow) 45px
+		);
+	}
+
+	50% {
+		background: repeating-linear-gradient(
+			45deg,
+			var(--main-color-purple) 15px,
+			#888 15px,
+			var(--main-color-yellow) 16px,
+			var(--main-color-yellow) 30px,
+			#888 30px,
+			var(--main-color-purple) 31px,
+			var(--main-color-purple) 45px
+		);
+	}
+`;
+
+const fadeInAndMoveDownAnimation = keyframes`
+	0% {
+		opacity: 0;
+		transform: translateY(-20px);
+	}
+	100% {
+		opacity: 1;
+		transform: translateY(0);
+	}
+`;
 
 const Container = styled.div`
 	width: 80%;
@@ -19,6 +58,9 @@ const Container = styled.div`
 		var(--main-color-yellow) 31px,
 		var(--main-color-yellow) 45px
 	);
+	animation:
+		${fadeInAndMoveDownAnimation} 500ms forwards,
+		${backgroundColorAnimation} 600ms infinite;
 	box-shadow: 10px 10px 20px rgba(0, 0, 0, 0.2);
 `;
 
