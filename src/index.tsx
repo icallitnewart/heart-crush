943e07c1582f043d3da3770280ca84ce8a561092
icallitnewart <icallitnewart@gmail.com>
2024-09-19 00:26:30 +0900

    Chore: PWA 설정 및 배포 테스트
    설명
    - PWA 설정에 필요한 관련 라이브러리 설치
    - manifest 파일 설정 및 이미지 파일 추가
    - 서비스 워커 설정 및 등록
    
    참고사항
    - 관련 라이브러리 설치시 tsconfig.json 에러가 발생하여 설정을 추가하였으나, 해결된 것인지는 확인 필요
    
    Related to: #39

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 import store, { persistor } from './redux/store';
 
@@ -19,3 +20,8 @@ root.render(
 		</Provider>
 	</React.StrictMode>,
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
